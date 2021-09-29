da490c18a401e179a72d0c62ab39695874d01600
elroykanye <elroykanye@gmail.com>
2021-09-29 12:04:29 +0100

    initial commit - ek

---
+++ src/main/java/com/gdscuba/membermssb/MemberMsSbApplication.java
@@ -0,0 +1,13 @@
+package com.gdscuba.membermssb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MemberMsSbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MemberMsSbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gdscuba/membermssb/MemberMsSbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdscuba.membermssb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberMsSbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
